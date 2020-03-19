4eddf02515819dcb9288fac84f875e8c8418c8d6
Daniel Ebbert <daniel@ebbert.nrw>
2020-03-19 11:10:10 +0000

    version 0.1.3

--- inst/doc/getting_started.R
+++ inst/doc/getting_started.R
@@ -1,25 +1,25 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 library(knitr)
 
-## ----oc_setup------------------------------------------------------------
+## ----oc_setup-----------------------------------------------------------------
 library(opencastR)
 
 Sys.setenv(OPENCAST_HOST = "https://legacy.opencast.org")
 Sys.setenv(OPENCAST_USERNAME = "admin")
 Sys.setenv(OPENCAST_PASSWORD = "opencast")
 
-## ----oc_api--------------------------------------------------------------
+## ----oc_api-------------------------------------------------------------------
 oc_api()
 
-## ----oc_api_content------------------------------------------------------
+## ----oc_api_content-----------------------------------------------------------
 api_test <- oc_api()
 api_test$content
 
-## ----oc_events_content---------------------------------------------------
+## ----oc_events_content--------------------------------------------------------
 events <- oc_list_events()
 kable(events$content)
 
